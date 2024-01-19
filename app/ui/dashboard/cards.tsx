55d153212744a83d41919379c57f88df7f883d13
AbenezerKitata <Abi4kit@gmail.com.com>
2024-01-18 21:52:14 -0800

    added speed insights

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -5,6 +5,10 @@ import {
   InboxIcon,
 } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
+import {
+  fetchCardData
+
+} from '@/app/lib/data';
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -14,18 +18,23 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
+  const FetchCardData = await fetchCardData();
+  const totalPaidInvoices = FetchCardData.totalPaidInvoices;
+  const totalPendingInvoices = FetchCardData.totalPendingInvoices;
+  const numberOfInvoices = FetchCardData.numberOfInvoices;
+  const numberOfCustomers = FetchCardData.numberOfCustomers;
   return (
     <>
       {/* NOTE: comment in this code when you get to this point in the course */}
 
-      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
-      /> */}
+      />
     </>
   );
 }
